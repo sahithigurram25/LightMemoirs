63ec5c8660511b87fd2b65c09f1d3635d403ab82
Sahithi Gurram <sahithiexplore@gmail.com>
2025-01-15 10:27:26 -0800

    changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const imageContainer = document.getElementById('imageContainer');
-imageContainer.style.backgroundImage = "url('Images/About/DSC06071.JPG')";
-imageContainer.style.backgroundSize = "cover";
-imageContainer.style.backgroundPosition = "center";
-imageContainer.textContent = ""; // Remove placeholder text
+imageContainer.innerHTML = '<img src="Images/About/DSC06071.JPG" alt="About image" style="width: 100%; height: auto;">';
+//imageContainer.style.backgroundImage = "url('Images/About/DSC06071.JPG')";
+//imageContainer.style.backgroundSize = "cover";
+//imageContainer.style.backgroundPosition = "center";
+//imageContainer.textContent = ""; // Remove placeholder text
