8f94025bdb3968ea53f31502e03bb962cc1e115b
sahithigurram25 <sahithiexplore@gmail.com>
2025-01-15 11:13:24 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 console.log("hello"); 
 const imageContainer = document.getElementById('imageContainer');
 console.log("image container got"); 
-imageContainer.innerHTML = '<img src="/LightMemoirs/Images/About/DSC06071.JPG" alt="Description of the image" class="image-placeholder">';
+//imageContainer.innerHTML = '<img src="/LightMemoirs/Images/About/DSC06071.JPG" alt="Description of the image" class="image-placeholder">';
+imageContainer.src = '/LightMemoirs/Images/About/DSC06071.JPG';
 console.log("image container set"); 
 //imageContainer.style.backgroundImage = "url('Images/About/DSC06071.JPG')";
 //imageContainer.style.backgroundSize = "cover";
