de4b50283498bd1799121753cd44eae9726eeed0
Sahithi Gurram <sahithiexplore@gmail.com>
2025-01-20 21:49:54 -0800

    adjusted positioning in about.html and customCSS.css
    Got a layout close to desired. Should still refine the code.

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 console.log("hello"); 
 const imageContainer = document.getElementById('imageContainer');
 console.log("image container got"); 
-imageContainer.innerHTML = '';
-imageContainer.innerHTML = '<img src="/LightMemoirs/Images/About/DSC06071.JPG" alt="Description of the image" class="image-placeholder">';
+//imageContainer.innerHTML = '';
+//const rootDir = '/LightMemoirs/';
+const rootDir = "";
+const imageDir = rootDir + "/Images/About/DSC06071.JPG";
+//imageContainer.className = "image-placeholder";
+//imageContainer.src = imageDir;
+console.log("image root" + imageContainer.src);
+imageContainer.innerHTML = '<img src="/Images/About/DSC06071.JPG" class="image-placeholder">';
 //imageContainer.src = '/LightMemoirs/Images/About/DSC06071.JPG';
-console.log("image container set"); 
+console.log("image container set");
 //imageContainer.style.backgroundImage = "url('Images/About/DSC06071.JPG')";
 //imageContainer.style.backgroundSize = "cover";
 //imageContainer.style.backgroundPosition = "center";
 //imageContainer.textContent = ""; // Remove placeholder text
+//document.body.appendChild(imageContainer);
