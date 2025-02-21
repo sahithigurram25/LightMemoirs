981a5d6feed34a52644dffedfbe426c579a187a4
Sahithi Gurram <sahithiexplore@gmail.com>
2025-02-21 13:47:36 -0800

    Update script.js
    script clean up and organisation

--- script.js
+++ script.js
@@ -1,59 +1,129 @@
-console.log("hello"); 
-const imageContainer = document.getElementById('aboutImage');
-console.log("image container got"); 
-//imageContainer.innerHTML = '';
-//const rootDir = '/LightMemoirs/';
-const rootDir = "";
-const imageDir = rootDir + "/Images/About/DSC06071.JPG";
-//imageContainer.className = "image-placeholder";
-//imageContainer.src = imageDir;
-console.log("image root" + imageContainer.src);
-imageContainer.innerHTML = '<img src="/Images/About/DSC06071.JPG" class="image-placeholder">';
-//imageContainer.src = '/LightMemoirs/Images/About/DSC06071.JPG';
-console.log("image container set");
-//imageContainer.style.backgroundImage = "url('Images/About/DSC06071.JPG')";
-//imageContainer.style.backgroundSize = "cover";
-//imageContainer.style.backgroundPosition = "center";
-//imageContainer.textContent = ""; // Remove placeholder text
-//document.body.appendChild(imageContainer);
-/**************Photography page**************************/
-//mobile config
-  //single column
-//tablet config
-  //2 columns
-//desktop config
-//3 columns
-const gallery = document.getElementById('photographyGallery');
+website = new Website();
+website.createWebsite();
 
-for (let i = 1; i <= 6; i++) {
-    //const img = document.createElement('img');
-    //img.src = `https://via.placeholder.com/150?text=Image+${i}`;
-    //img.alt = `Placeholder ${i}`;
-    //gallery.appendChild(img);
+
+class Website{
+
+    home = new Home();
+    about = new About();
+    photography = new Photography();
+    videography = new Videography();
+    
+    createWebsite(){
+        const isDesktop = adjustViewport();
+        window.addEventListener("resize", () => {
+            if (isDesktop) {
+                if (window.innerWidth > 1920) resizeViewport("1920px", window.innerHeight,"about");
+            }
+            else resizeViewport(window.innerWidth, window.innerHeight,"about");
+        });
+
+        setHeader();
+        setFooter();
+        this.about.createPage();
+        this.home.createPage();
+        this.photography.createPage();
+        this.videography.createPage();
+        console.log("website created");
+    }
+
+    adjustViewport() {
+        const userAgent = navigator.userAgent.toLowerCase();
+    
+        if (/mobile|android|iphone|ipod|blackberry|opera mini|iemobile|wpdesktop|tablet|ipad/.test(userAgent)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    resizeViewport(width, height, elementId) {
+        let container = document.getElementsByClassName(elementId)
+        container.style.width = width;
+        container.style.height = height;    
+    }
+
+    setHeader(){
+
+    }
+
+    setFooter(){
+
+    }
 }
-function resizeViewport(width, height, elementId) {
-    let container = document.getElementsByClassName(elementId)
-    container.style.width = width;
-    container.style.height = height;    
+//Home page
+class Home{
+    createPage(){
+        console.log("home page created");
+    }
+}
+//About Page
+class About{
+    imagePath;
+    infoText;
+
+    createPage(){
+        this.setProfileImage();
+        this.setProfileInfo();
+        this.setContactInfo();
+        console.log("about page created");
+    }
+
+    setProfileImage(){
+        const imageContainer = document.getElementById('aboutImage');
+        imageContainer.innerHTML = '<img src="/Images/About/DSC06071.JPG" class="image-placeholder">';
+    }
+    
+    setProfileInfo(){
+    
+    }
+
+    setContactInfo(){
+
+    }
+
 }
 
 
-function adjustViewport() {
-    const userAgent = navigator.userAgent.toLowerCase();
+//Photography Page
+class Photography{
 
-    if (/mobile|android|iphone|ipod|blackberry|opera mini|iemobile|wpdesktop|tablet|ipad/.test(userAgent)) {
-        return false;
-    } else {
-        return true;
+    createPage(){
+        this.setLayout();
+        console.log("photography page created");
+    }
+
+    setLayout(){
+        const gallery = document.getElementById('photographyGallery');
+        for (let i = 1; i <= 6; i++) {
+            //const img = document.createElement('img');
+            //img.src = `https://via.placeholder.com/150?text=Image+${i}`;
+            //img.alt = `Placeholder ${i}`;
+            //gallery.appendChild(img);
+        }
+    }
+    
+    updatePhotoDatabase(){
+    
     }
 }
 
-//console.log("Device size:", adjustViewport("aboutContainer"));
-console.log("window width" + window.innerWidth);
-const isDesktop = adjustViewport();
-window.addEventListener("resize", () => {
-    if (isDesktop) {
-        if (window.innerWidth > 1920) resizeViewport("1920px", window.innerHeight,"about");
+
+//Videography page
+class Videography{
+
+    createPage(){
+        console.log("videography page created");
     }
-    else resizeViewport(window.innerWidth, window.innerHeight,"about");
-});
+
+    setLayout(){
+
+    }
+
+    updateVideoDatabase(){
+
+    }
+    
+}
+
+
